d9e3140e7e8d46b77857af1437473c47ec7be3ee
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-07-03 18:04:55 +0200

    feat_search_eventlistener

--- src/main.js
+++ src/main.js
@@ -1,23 +1,21 @@
 import './style.css';
-import Sidebar from "./components/Sidebar/Sidebar.js"
-import Navbar from "./components/Navbar/Navbar.js"
+import Sidebar from './components/Sidebar/Sidebar.js'
+import Navbar from './components/Navbar/Navbar.js'
 
-const ACCESS_KEY = "LZU19IMPHQJS1SlRC0GuDfxpl9YOROvq6BgBk7C0OEg";
+const ACCESS_KEY = 'LZU19IMPHQJS1SlRC0GuDfxpl9YOROvq6BgBk7C0OEg';
 const app = document.getElementById('app');
-const searchBtn = document.getElementById('Buscar');
-const searchInput = document.getElementById('search-input');
-
 app.innerHTML = `${Sidebar()}
 ${Navbar()}
 `
-
+const searchBtn = document.getElementById("Buscar");
+const searchInput = document.getElementById("search-input");
 
 //HACEMOS LA FUNCION CON LA PROMESA CON COGERÁ LAS FOTOS DE LA API.
 
 
-const getImages = async () => {
+const getImages = async (keyword) => {
   const res = await fetch(
-    `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&query=cat&page=1&per_page=20`
+    `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&query=${keyword}&page=1&per_page=20`
   );
   const data = await res.json();
   mapImages(data.results);
@@ -38,20 +36,26 @@ const mapImages = (images) =>{
 //CREAMOS LA FUNCIÓN QUE PINTARÁ LAS FOTOS EN LA APP. 
 
 const printImages = (images) => {
-  const gallery = document.getElementById("image-gallery");
+  const gallery = document.getElementById('image-gallery');
+  gallery.innerHTML = '';
   images.forEach(image => {
     const li = document.createElement('li');
     li.innerHTML = `
-    <a href="${image.raw_image}" target="_blank" rel="noopener noreferrer">
-    <img src="${image.image}" alt="${image.alt}"/>
+    <a href='${image.raw_image}' target='_blank' rel='noopener noreferrer'>
+    <img src='${image.image}' alt='${image.alt}'/>
     </a>
     `;
     gallery.appendChild(li);
   });
 }
 
+//AÑADO EL EVENTO QUE ESCUCHA EL CLICK DEL BOTON BUSCAR CON EL VALOR DEL IMPUT.
+
+searchBtn.addEventListener('click', () =>{
+  getImages(searchInput.value);
+});
 
 
-window.addEventListener("DOMContentLoaded", () =>{
-  getImages();
+window.addEventListener('DOMContentLoaded', () =>{
+  getImages("cat");
 });
